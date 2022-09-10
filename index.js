ed1ae667546dcaac8bd3c6bd3a774cf60a9aac6f
unknown <sfqtuhin04@gmail.com>
2022-09-10 22:31:20 +0600

    database updating

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ async function run(){
         res.send(services);
         });
 
+
+        app.get('/booking', async (req, res)=>{
+          const patient = req.query.patient;
+          const query = {patient: patient};
+          const bookings = await bookingCollection.find(query).toArray();
+          res.send(bookings);
+        })
+        
         app.post('/booking', async (req, res)=>{
           const booking = req.body;
           const query = {treatment: booking.treatment, patient:booking.patient, slot:booking.slot, date:booking.date};
